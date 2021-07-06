0e8c430a8e463f585448680babc119dbe38b1c69
Сосов Максим Петрович [B] <OUT-Sosov-MP@sigma.sbrf.ru>
2021-07-06 08:48:19 +0300

    add color to scatter_plot.py

--- scatter_plot.py
+++ scatter_plot.py
@@ -9,34 +9,60 @@ def check_argv(argv):
 
 def fill_data(path):
 	title = 0
-	feature = {'Astronomy':[], 'Defense Against the Dark Arts':[]}
+	feature = {}
 	with open(path) as file:
 		for line in file:
 			if not title:
 				title = line[:-1].split(',')
 				for i, data in enumerate(title):
+					if data == 'Hogwarts House':
+						house = i
 					if data == 'Astronomy':
 						astronomy = i
 					if data == 'Defense Against the Dark Arts':
 						darkart = i
 				continue
 			data = line[:-1].split(',')
-			if data[astronomy] == '':
-				feature['Astronomy'].append('NaN')
-			else:
-				feature['Astronomy'].append(float(data[astronomy]))
-			if data[darkart] == '':
-				feature['Defense Against the Dark Arts'].append('NaN')
-			else:
-				feature['Defense Against the Dark Arts'].append(float(data[darkart]))
+			try:
+				for f in feature[data[house]]:
+					if data[astronomy] == '':
+						feature[data[house]]['Astronomy'].\
+							append('NaN')
+					else:
+						feature[data[house]]['Astronomy'].\
+							append(float(data[astronomy]))
+					if data[darkart] == '':
+						feature[data[house]]['DefDarkArt'].\
+							append('NaN')
+					else:
+						feature[data[house]]['DefDarkArt'].\
+							append(float(data[darkart]))
+			except KeyError:
+				feature[data[house]] = {}
+				if data[astronomy] == '':
+					feature[data[house]]['Astronomy'] =\
+						['NaN']
+				else:
+					feature[data[house]]['Astronomy'] =\
+						[float(data[astronomy])]
+				if data[darkart] == '':
+					feature[data[house]]['DefDarkArt'] =\
+						['NaN']
+				else:
+					feature[data[house]]['DefDarkArt'] =\
+						[float(data[darkart])]
 	return feature
 
 def draw_graph(feature):
 	plt.figure(figsize=(18, 10))
-	plt.scatter(feature['Astronomy'], feature['Defense Against the Dark Arts'], s=10)
+	for house in feature:
+		plt.scatter(feature[house]['Astronomy'],
+					feature[house]['DefDarkArt'],
+					label=house, alpha=0.5, s=10)
 	plt.title('Similar features')
 	plt.xlabel('Astronomy')
 	plt.ylabel('Defense Against the Dark Arts')
+	plt.legend(loc='upper right')
 	plt.show()
 
 def main(argv):
